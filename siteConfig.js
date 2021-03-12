a5dd9ac97c4ef9e0854a3e4dce8b4965aac82b51
pythonidae-lupus <34830926+pythonidae-lupus@users.noreply.github.com>
2021-03-12 22:39:02 +0530

    Updated URL

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'London',
   shortName: 'London',
   description: 'If you have knowledge, let others light their candles in it - Margaret Fuller',
-  url: 'https://ysblog.netlify.com', // Domain of your site without prefix!
+  url: 'https://yajurs-blog.netlify.app', // Domain of your site without prefix!
   prefix: '/',
   author: 'Imed Adel',
   twitter: 'Imed_Adel'
